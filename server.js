75b2c35060b2e1cd46c5c35b8bc1ce21dc9b815d
skylines_rgood <geraldistheking@gmail.com>
2026-09-07 13:30:11 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 import http from "http";
-import { WebSocketServer } from "ws";
-import { WispServer } from "@mercuryworkshop/wisp-server-node";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("Sandstone Wisp Server Online");
-});
-
-const wss = new WebSocketServer({ noServer: true });
-
-const wisp = new WispServer();
-
-server.on("upgrade", (request, socket, head) => {
-  if (request.url === "/") {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wisp.handleConnection(ws);
-    });
-  }
 });
 
 server.listen(process.env.PORT || 8080, () => {
